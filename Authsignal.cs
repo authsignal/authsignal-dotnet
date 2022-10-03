ad516e5605ca2c53fbe17337ffe54be4952e3723
Chris Fisher <chris.i.fisher@gmail.com>
2022-10-03 13:01:42 +1300

    [AUT-262] Add validateChallenge method

--- Authsignal.cs
+++ Authsignal.cs
@@ -5,6 +5,9 @@ namespace Authsignal
   using System.Text;
   using System.Text.Json;
   using System.Text.Json.Serialization;
+  using System.IdentityModel.Tokens.Jwt;
+  using System.Security.Cryptography;
+  using Microsoft.IdentityModel.Tokens;
 
   public class AuthsignalClient
   {
@@ -16,11 +19,13 @@ namespace Authsignal
       DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
     };
 
-    HttpClient _httpClient;
+    string _secret;
     string? _redirectUrl;
+    HttpClient _httpClient;
 
     public AuthsignalClient(string secret, string? redirectUrl = null, string baseAddress = DEFAULT_BASE_ADDRESS)
     {
+      _secret = secret;
       _redirectUrl = redirectUrl;
 
       _httpClient = new HttpClient()
@@ -31,7 +36,7 @@ namespace Authsignal
       _httpClient.DefaultRequestHeaders.Add("Authorization", $"Basic {Base64Encode($"{secret}:")}");
     }
 
-    public async Task<UserResponse?> GetUser(UserRequest request)
+    public async Task<UserResponse> GetUser(UserRequest request)
     {
       var path = $"users/{request.UserId}";
 
@@ -44,19 +49,16 @@ namespace Authsignal
     {
       var path = $"users/{request.UserId}/actions/{request.Action}";
 
-      var body = new TrackRequestBody
-      {
-        Email = request.Email,
-        IdempotencyKey = request.IdempotencyKey,
-        RedirectUrl = request.RedirectUrl ?? this._redirectUrl,
-        IpAddress = request.IpAddress,
-        UserAgent = request.UserAgent,
-        DeviceId = request.DeviceId,
-        RedirectToSettings = request.RedirectToSettings
-      };
+      var body = new TrackRequestBody(
+        Email: request.Email,
+        IdempotencyKey: request.IdempotencyKey,
+        RedirectUrl: request.RedirectUrl ?? this._redirectUrl,
+        IpAddress: request.IpAddress,
+        UserAgent: request.UserAgent,
+        DeviceId: request.DeviceId,
+        RedirectToSettings: request.RedirectToSettings);
 
       var response = await _httpClient.PostAsJsonAsync(path, body, serializeOptions);
-
 
       var trackResponse = await response.Content.ReadFromJsonAsync<TrackResponse>();
 
@@ -84,6 +86,33 @@ namespace Authsignal
       }
     }
 
+    public async Task<ValidateChallengeResponse> ValidateChallenge(ValidateChallengeRequest request)
+    {
+      var jwtToken = ValidateToken(request.Token, _secret);
+
+      var userId = jwtToken.Subject;
+      var json = jwtToken.Claims.First(x => x.Type == "other").Value;
+      var other = JsonSerializer.Deserialize<JwtOtherData>(json, serializeOptions);
+      var idempotencyKey = other?.IdempotencyKey;
+      var actionCode = other?.ActionCode;
+
+      if (userId == null || idempotencyKey == null || actionCode == null)
+      {
+        throw new Exception("Invalid token");
+      }
+
+      if (request.UserId != userId)
+      {
+        throw new Exception("Invalid user");
+      }
+
+      var action = await GetAction(new ActionRequest(userId, actionCode, idempotencyKey));
+
+      var success = action?.State == UserActionState.CHALLENGE_SUCCEEDED;
+
+      return new ValidateChallengeResponse(success, action?.State);
+    }
+
     public async Task<AuthenticatorResponse> EnrollVerifiedAuthenticator(AuthenticatorRequest request)
     {
       var path = $"users/{request.UserId}/authenticators";
@@ -91,8 +120,7 @@ namespace Authsignal
       var body = new AuthenticatorRequestBody(
         OobChannel: request.OobChannel,
         Email: request.Email,
-        PhoneNumber: request.PhoneNumber
-        );
+        PhoneNumber: request.PhoneNumber);
 
       var response = await _httpClient.PostAsJsonAsync(path, body, serializeOptions);
 
@@ -105,9 +133,7 @@ namespace Authsignal
     {
       var path = $"users/email/{request.Email}/challenge";
 
-      var body = new EmailRequestBody(
-        RedirectUrl: request.RedirectUrl
-        );
+      var body = new EmailRequestBody(RedirectUrl: request.RedirectUrl);
 
       var response = await _httpClient.PostAsJsonAsync(path, body, serializeOptions);
 
@@ -120,6 +146,26 @@ namespace Authsignal
     {
       byte[] textAsBytes = Encoding.UTF8.GetBytes(textToEncode);
       return Convert.ToBase64String(textAsBytes);
+    }
+
+    private static JwtSecurityToken ValidateToken(string token, string secret)
+    {
+      var tokenHandler = new JwtSecurityTokenHandler();
+
+      var hmac = new HMACSHA256(Encoding.ASCII.GetBytes(secret));
+      var securityKey = new SymmetricSecurityKey(hmac.Key);
+
+      tokenHandler.ValidateToken(token, new TokenValidationParameters
+      {
+        IssuerSigningKey = securityKey,
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+      }, out SecurityToken validatedToken);
+
+      var jwtToken = (JwtSecurityToken)validatedToken;
+
+      return jwtToken;
     }
   }
 }
