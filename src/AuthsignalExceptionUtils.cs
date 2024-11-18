ad4ec6317f2e27c4167a32dadf673c21fb8b925f
Chris Fisher <chris.i.fisher@gmail.com>
2024-11-18 14:47:55 +1300

    wip

---
+++ src/AuthsignalExceptionUtils.cs
@@ -0,0 +1,26 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Authsignal;
+
+public static class AuthsignalExceptionUtils
+{
+    private static readonly JsonSerializerOptions serializerOptions = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    public static async Task<AuthsignalException> NewResponseException(HttpResponseMessage response)
+    {
+        var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+        var errorResponse = JsonSerializer.Deserialize<AuthsignalErrorResponse>(content, serializerOptions)!;
+
+        var errorMessage = errorResponse.ErrorDescription != null && errorResponse.ErrorDescription.Length > 0
+            ? errorResponse.ErrorDescription
+            : errorResponse.Error;
+
+        return new AuthsignalException((int)response.StatusCode, errorMessage);
+    }
+}
